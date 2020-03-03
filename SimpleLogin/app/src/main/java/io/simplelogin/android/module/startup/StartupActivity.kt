7ecb80b2ffee330b89889ddde2495fd742892127
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-03-03 14:41:18 +0100

    Init empty project with app icon

---
+++ SimpleLogin/app/src/main/java/io/simplelogin/android/module/startup/StartupActivity.kt
@@ -0,0 +1,8 @@
+package io.simplelogin.android.module.startup
+
+import io.simplelogin.android.utils.baseclass.BaseAppCompatActivity
+
+
+class StartupActivity : BaseAppCompatActivity()  {
+
+}
---
+++ SimpleLogin/app/src/main/java/io/simplelogin/android/utils/baseclass/BaseAppCompatActivity.kt
@@ -0,0 +1,7 @@
+package io.simplelogin.android.utils.baseclass
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseAppCompatActivity : AppCompatActivity() {
+
+}
