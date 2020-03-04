a8a4e6eef72c2a99d02fb15559f035a84d715e61
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-03-04 18:02:20 +0100

    Get Google accessToken

--- SimpleLogin/app/src/main/java/io/simplelogin/android/module/login/LoginActivity.kt
+++ SimpleLogin/app/src/main/java/io/simplelogin/android/module/login/LoginActivity.kt
@@ -9,16 +9,24 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import io.simplelogin.android.R
 import io.simplelogin.android.databinding.ActivityLoginBinding
 import io.simplelogin.android.utils.baseclass.BaseAppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.view.*
 
 class LoginActivity : BaseAppCompatActivity() {
     companion object {
         lateinit var binding: ActivityLoginBinding
             private set
+        private const val RC_SIGN_IN = 0 // Request code for Google Sign In
     }
 
-    lateinit var callbackManager: CallbackManager
+    private lateinit var facebookCallbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +34,13 @@ class LoginActivity : BaseAppCompatActivity() {
         setContentView(binding.root)
 
         binding.facebookButton.setOnClickListener { loginWithFacebook() }
+        binding.googleButton.setOnClickListener { loginWithGoogle() }
     }
 
-    // Facebook login
     private fun loginWithFacebook() {
-        callbackManager = CallbackManager.Factory.create()
+        facebookCallbackManager = CallbackManager.Factory.create()
         LoginManager.getInstance().logInWithReadPermissions(this, setOf("email"))
-        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+        LoginManager.getInstance().registerCallback(facebookCallbackManager, object : FacebookCallback<LoginResult> {
             override fun onSuccess(result: LoginResult?) {
                 result?.let {
                     Log.d("fb", it.accessToken.token)
@@ -40,18 +48,44 @@ class LoginActivity : BaseAppCompatActivity() {
             }
 
             override fun onCancel() {
-                Toast.makeText(this@LoginActivity, "Facebook login cancelled", Toast.LENGTH_SHORT)
+                Toast.makeText(this@LoginActivity, "Facebook login cancelled", Toast.LENGTH_SHORT).show()
             }
 
             override fun onError(error: FacebookException?) {
-                Toast.makeText(this@LoginActivity, "Facebook login failed: ${error.toString()}", Toast.LENGTH_SHORT)
+                Toast.makeText(this@LoginActivity, "Facebook login failed: ${error.toString()}", Toast.LENGTH_SHORT).show()
             }
 
         })
     }
 
+    private fun loginWithGoogle() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.google_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        callbackManager.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            RC_SIGN_IN -> {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                handleGoogleSignInResult(task)
+            }
+            else -> facebookCallbackManager.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun handleGoogleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            Log.d("google", account?.idToken)
+        } catch (e: ApiException) {
+            Toast.makeText(this, "Google sign in failed: ${e.localizedMessage}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
