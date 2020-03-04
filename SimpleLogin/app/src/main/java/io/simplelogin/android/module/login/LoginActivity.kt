cc88afc4928c61ca2bfd7e6eba80d63d73bcb892
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-03-04 15:13:29 +0100

    Get Facebook accessToken

--- SimpleLogin/app/src/main/java/io/simplelogin/android/module/login/LoginActivity.kt
+++ SimpleLogin/app/src/main/java/io/simplelogin/android/module/login/LoginActivity.kt
@@ -1,6 +1,14 @@
 package io.simplelogin.android.module.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import io.simplelogin.android.databinding.ActivityLoginBinding
 import io.simplelogin.android.utils.baseclass.BaseAppCompatActivity
 
@@ -10,9 +18,40 @@ class LoginActivity : BaseAppCompatActivity() {
             private set
     }
 
+    lateinit var callbackManager: CallbackManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.facebookButton.setOnClickListener { loginWithFacebook() }
+    }
+
+    // Facebook login
+    private fun loginWithFacebook() {
+        callbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().logInWithReadPermissions(this, setOf("email"))
+        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                result?.let {
+                    Log.d("fb", it.accessToken.token)
+                }
+            }
+
+            override fun onCancel() {
+                Toast.makeText(this@LoginActivity, "Facebook login cancelled", Toast.LENGTH_SHORT)
+            }
+
+            override fun onError(error: FacebookException?) {
+                Toast.makeText(this@LoginActivity, "Facebook login failed: ${error.toString()}", Toast.LENGTH_SHORT)
+            }
+
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 }
