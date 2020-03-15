5c6d985bd960065687609096f5573caeea5f183c
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-03-15 22:31:00 +0100

    Mock AliasFragment toolbar menu & tab selection

--- SimpleLogin/app/src/main/java/io/simplelogin/android/module/alias/AliasFragment.kt
+++ SimpleLogin/app/src/main/java/io/simplelogin/android/module/alias/AliasFragment.kt
@@ -1,13 +1,18 @@
 package io.simplelogin.android.module.alias
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.tabs.TabLayout
+import io.simplelogin.android.R
 import io.simplelogin.android.databinding.FragmentAliasBinding
 import io.simplelogin.android.utils.baseclass.BaseFragment
 
-class AliasFragment : BaseFragment() {
+class AliasFragment : BaseFragment(), Toolbar.OnMenuItemClickListener, TabLayout.OnTabSelectedListener {
     lateinit var binding: FragmentAliasBinding
 
     override fun onCreateView(
@@ -16,9 +21,50 @@ class AliasFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAliasBinding.inflate(inflater)
-
         binding.toolbar.setNavigationOnClickListener { showLeftMenu() }
-
+        binding.toolbar.setOnMenuItemClickListener(this)
+        binding.tabLayout.addOnTabSelectedListener(this)
         return binding.root
+    }
+
+    // Toolbar.OnMenuItemClickListener
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.searchMenuItem -> {
+                Log.d("menu", "search")
+            }
+
+            R.id.randomMenuItem -> {
+                Log.d("menu", "shuffle")
+            }
+
+            R.id.addMenuItem -> {
+                Log.d("menu", "add")
+            }
+        }
+
+        return true
+    }
+
+    // TabLayout.OnTabSelectedListener
+    override fun onTabReselected(tab: TabLayout.Tab?) = Unit
+    override fun onTabUnselected(tab: TabLayout.Tab?) = Unit
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        when (tab?.position) {
+            0 -> {
+                // All
+                Log.d("tab", "all")
+            }
+
+            1 -> {
+                // All
+                Log.d("tab", "active")
+            }
+
+            2 -> {
+                // All
+                Log.d("tab", "inactive")
+            }
+        }
     }
 }
