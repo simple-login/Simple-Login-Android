c77c175e2fe714efbe333899f928fd964fad5b8e
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-03-16 23:16:16 +0100

    AliasViewHolder: bind background color and switch state

--- SimpleLogin/app/src/main/java/io/simplelogin/android/viewholder/AliasViewHolder.kt
+++ SimpleLogin/app/src/main/java/io/simplelogin/android/viewholder/AliasViewHolder.kt
@@ -1,8 +1,11 @@
 package io.simplelogin.android.viewholder
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import io.simplelogin.android.R
 import io.simplelogin.android.databinding.RecyclerItemAliasBinding
 import io.simplelogin.android.utils.model.Alias
 
@@ -15,7 +18,16 @@ class AliasViewHolder(private val binding: RecyclerItemAliasBinding) : RecyclerV
         }
     }
 
+    private val context: Context by lazy { binding.root.context }
+
     fun bind(alias: Alias) {
         binding.emailTextView.text = alias.email
+
+        binding.enabledSwitch.isEnabled = alias.enabled
+        if (alias.enabled) {
+            binding.rootRelativeLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.colorMediumGray))
+        } else {
+            binding.rootRelativeLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.colorWhite))
+        }
     }
 }
