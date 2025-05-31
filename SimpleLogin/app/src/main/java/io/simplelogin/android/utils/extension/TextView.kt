5c39d00e6294504964588d561642b6e5b95618bc
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-05-31 09:22:09 +0200

    chore: remove old project

--- SimpleLogin/app/src/main/java/io/simplelogin/android/utils/extension/TextView.kt
+++
@@ -1,8 +0,0 @@
-package io.simplelogin.android.utils.extension
-
-import android.widget.TextView
-import androidx.annotation.DrawableRes
-
-fun TextView.setDrawableStart(@DrawableRes id: Int = 0) {
-    setCompoundDrawablesRelativeWithIntrinsicBounds(id, 0, 0, 0)
-}
--- SimpleLogin/app/src/main/java/io/simplelogin/android/utils/interfaces/Reversable.kt
+++
@@ -1,7 +0,0 @@
-package io.simplelogin.android.utils.interfaces
-
-interface Reversable {
-    val email: String
-    val reverseAlias: String
-    val reverseAliasAddress: String
-}
