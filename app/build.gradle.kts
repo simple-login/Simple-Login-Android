868c4013321a7cbdad1e2393a2d2e299b6c99dc6
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-05-31 10:20:58 +0200

    feat: create playstore and fdroid flavors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,22 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    flavorDimensions += "version"
+    productFlavors {
+        create("playstore") {
+            dimension = "version"
+            isDefault = true
+        }
+
+        create("fdroid") {
+            dimension = "version"
+            applicationIdSuffix = ".fdroid"
+        }
     }
 
     buildTypes {
@@ -36,6 +49,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
