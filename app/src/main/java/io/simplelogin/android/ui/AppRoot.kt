6914f85da358ba3e3884fd2c18896cc4d4bb6caa
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-06-16 22:38:08 +0200

    feat: use DSL to render nav destination & improve AppRootViewModel's readability

--- app/src/main/java/io/simplelogin/android/ui/AppRoot.kt
+++ app/src/main/java/io/simplelogin/android/ui/AppRoot.kt
@@ -14,8 +14,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.NavBackStack
-import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entry
+import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSavedStateNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
@@ -36,24 +37,24 @@ import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import javax.inject.Inject
 
-sealed class AppRootDestination: NavKey {
-    @Serializable
-    object Initialization: AppRootDestination()
-    @Serializable
-    object LogIn: AppRootDestination()
-    @Serializable
-    data class Home(val apiKey: String): AppRootDestination()
-}
+@Serializable
+data object InitializationDestination: NavKey
+
+@Serializable
+data object LogInDestination: NavKey
+
+@Serializable
+data class HomeDestination(val apiKey: String): NavKey
 
 @Composable
 fun AppRoot(modifier: Modifier = Modifier,
             snackbarHostState: SnackbarHostState,
             viewModel: AppRootViewModel
 ) {
-    val backStack = rememberNavBackStack(AppRootDestination.Initialization)
+    val backStack = rememberNavBackStack(InitializationDestination)
     val baseUrl by viewModel.baseUrl.collectAsState()
 
-    LaunchedEffect(Unit) {
+    LaunchedEffect(backStack) {
         viewModel.setNavBackStack(backStack)
         viewModel.setSnackbarHostState(snackbarHostState)
     }
@@ -66,45 +67,30 @@ fun AppRoot(modifier: Modifier = Modifier,
             rememberViewModelStoreNavEntryDecorator(),
             rememberSceneSetupNavEntryDecorator()
         ),
-        entryProvider = { navKey ->
-            if (navKey is AppRootDestination) {
-                return@NavDisplay when(navKey) {
-                    AppRootDestination.Initialization -> {
-                        NavEntry(navKey) {
-                            // Dummy empty screen while deciding if the user is logged in or not
-                            // Rely on splash screen and show nothing here
-                        }
-                    }
+        entryProvider = entryProvider {
+            entry<InitializationDestination> {}
 
-                    AppRootDestination.LogIn -> {
-                        NavEntry(navKey) {
-                            LoginScreen(
-                                modifier = modifier,
-                                appVersion = viewModel.appVersion,
-                                baseUrl = baseUrl,
-                                onBaseUrlChange = viewModel::updateBaseUrl,
-                                onLoginClick = viewModel::logIn,
-                                onLoginWithProtonClick = viewModel::logInWithProton,
-                                onLoginWithApiKeyClick = viewModel::updateApiKey,
-                                onForgotPassword = viewModel::resetPassword,
-                                onSignUp = viewModel::createAccount,
-                                onResendActivationCode = viewModel::resendActivationCode
-                            )
-                        }
-                    }
+            entry<LogInDestination> {
+                LoginScreen(
+                    modifier = modifier,
+                    appVersion = viewModel.appVersion,
+                    baseUrl = baseUrl,
+                    onBaseUrlChange = viewModel::updateBaseUrl,
+                    onLoginClick = viewModel::logIn,
+                    onLoginWithProtonClick = viewModel::logInWithProton,
+                    onLoginWithApiKeyClick = viewModel::updateApiKey,
+                    onForgotPassword = viewModel::resetPassword,
+                    onSignUp = viewModel::createAccount,
+                    onResendActivationCode = viewModel::resendActivationCode
+                )
+            }
 
-                    is AppRootDestination.Home -> {
-                        NavEntry(navKey) {
-                            HomeScreen(
-                                modifier = modifier,
-                                apiKey = navKey.apiKey,
-                                onLogOutClick = viewModel::logOut,
-                            )
-                        }
-                    }
-                }
-            } else {
-                throw RuntimeException("NavKey must be of type AppRootDestination")
+            entry<HomeDestination> { key ->
+                HomeScreen(
+                    modifier = modifier,
+                    apiKey = key.apiKey,
+                    onLogOutClick = viewModel::logOut,
+                )
             }
         }
     )
@@ -119,38 +105,35 @@ class AppRootViewModel @Inject constructor(
     private val _isAppReady = MutableStateFlow(false)
     val isAppReady = _isAppReady.asStateFlow()
 
-    private var _navBackStack: NavBackStack? = null
     private val _baseUrl = MutableStateFlow<String>(Constants.DEFAULT_BASE_URL)
     val baseUrl = _baseUrl.asStateFlow()
 
-    private var _snackbarHostState: SnackbarHostState? = null
-
     val appVersion = "v${BuildConfig.VERSION_NAME}-${BuildConfig.FLAVOR}"
 
-    init {
+    fun setNavBackStack(navBackStack: NavBackStack) {
         viewModelScope.launch {
             userSessionPreferences.data
                 .collect {
                     _isAppReady.value = true
                     _baseUrl.value = it.baseUrl
-                    assert(_navBackStack != null) { "NavBackStack is not set" }
-                    _navBackStack?.apply {
+                    navBackStack.apply {
                         clear()
                         val apiKey = it.apiKey
                         if (apiKey != null) {
-                            add(AppRootDestination.Home(apiKey))
+                            add(HomeDestination(apiKey))
                         } else {
-                            add(AppRootDestination.LogIn)
+                            add(LogInDestination)
                         }
                     }
                 }
         }
+    }
 
+    fun setSnackbarHostState(snackbarHostState: SnackbarHostState) {
         viewModelScope.launch {
             snackbarManager.configuration
                 .collect { configuration ->
-                    assert(_snackbarHostState != null) { "SnackbarHostState is not set" }
-                    val result = _snackbarHostState?.showSnackbar(
+                    val result = snackbarHostState.showSnackbar(
                         message = configuration.message,
                         actionLabel = configuration.action?.label,
                         withDismissAction = configuration.duration == SnackbarDuration.Indefinite,
@@ -166,14 +149,6 @@ class AppRootViewModel @Inject constructor(
                     }
                 }
         }
-    }
-
-    fun setNavBackStack(navBackStack: NavBackStack) {
-        _navBackStack = navBackStack
-    }
-
-    fun setSnackbarHostState(snackbarHostState: SnackbarHostState) {
-        _snackbarHostState = snackbarHostState
     }
 
     fun logIn() {
