aa66798db3b583cb6e0d40a57eacf914c859ac9f
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-07-10 09:16:37 +0200

    feat: check URL validity

--- app/src/main/java/io/simplelogin/android/ui/login/EditBaseUrlDialog.kt
+++ app/src/main/java/io/simplelogin/android/ui/login/EditBaseUrlDialog.kt
@@ -1,16 +1,23 @@
 package io.simplelogin.android.ui.login
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Cancel
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -18,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import io.simplelogin.android.R
+import io.simplelogin.android.util.isValidUrl
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,6 +36,8 @@ fun EditBaseUrlDialog(
     onUseDefault: () -> Unit
 ) {
     var updatedBaseUrl by rememberSaveable { mutableStateOf(baseUrl) }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
 
     AlertDialog(
         onDismissRequest = onDismiss,
@@ -41,7 +51,24 @@ fun EditBaseUrlDialog(
                     onValueChange = { updatedBaseUrl = it },
                     placeholder = { Text(stringResource(R.string.api_url)) },
                     singleLine = true,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Uri)
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Uri),
+                    isError = !updatedBaseUrl.isValidUrl(),
+                    interactionSource = interactionSource,
+                    supportingText = {
+                        if (!updatedBaseUrl.isEmpty() && !updatedBaseUrl.isValidUrl()) {
+                            Text(stringResource(R.string.invalid_url_error))
+                        }
+                    },
+                    trailingIcon = {
+                        if (isFocused) {
+                            IconButton(onClick = { updatedBaseUrl = "" }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Cancel,
+                                    contentDescription = stringResource(R.string.clear_api_url)
+                                )
+                            }
+                        }
+                    }
                 )
             }
         },
@@ -51,6 +78,7 @@ fun EditBaseUrlDialog(
                 horizontalAlignment = Alignment.End
             ) {
                 TextButton(
+                    enabled = updatedBaseUrl.isValidUrl(),
                     onClick = { onSave(updatedBaseUrl) }
                 ) {
                     Text(stringResource(R.string.save))
--- app/src/main/java/io/simplelogin/android/util/StringUtils.kt
+++ app/src/main/java/io/simplelogin/android/util/StringUtils.kt
@@ -5,3 +5,5 @@ import android.util.Patterns
 fun String.isValidEmail() = Patterns.EMAIL_ADDRESS.matcher(this).matches()
 
 fun String.isValidPassword() = count() >= 8
+
+fun String.isValidUrl() = Patterns.WEB_URL.matcher(this).matches()
