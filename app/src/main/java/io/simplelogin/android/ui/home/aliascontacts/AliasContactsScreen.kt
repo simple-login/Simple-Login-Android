e2074aec0d138a6217eaf56aa5d9661e3c235828
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2026-03-11 22:00:44 +0100

    feat: handle no contacts

--- app/src/main/java/io/simplelogin/android/ui/home/aliascontacts/AliasContactsScreen.kt
+++ app/src/main/java/io/simplelogin/android/ui/home/aliascontacts/AliasContactsScreen.kt
@@ -35,8 +35,10 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -139,8 +141,19 @@ fun AliasContactsScreen(
                     state = listState,
                     contentPadding = PaddingValues(Spacing.regular)
                 ) {
-                    stickyHeader {
-                        SettingsHeader(text = stringResource(R.string.contacts))
+                    if (state.contacts.isNotEmpty()) {
+                        stickyHeader {
+                            SettingsHeader(text = stringResource(R.string.contacts))
+                        }
+                    }
+
+                    if (!state.isRefreshing && !state.isLoadingMore && state.contacts.isEmpty()) {
+                        item {
+                            Text(
+                                text = stringResource(R.string.no_contacts),
+                                color = MaterialTheme.colorScheme.secondary
+                            )
+                        }
                     }
 
                     val lastIndex = state.contacts.lastIndex
